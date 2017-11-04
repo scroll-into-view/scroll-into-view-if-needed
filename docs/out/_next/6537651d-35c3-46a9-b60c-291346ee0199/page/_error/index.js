4f93de2d39749714329aa868f12e1f6d0c00f3c5
Stian Didriksen <stipsan@gmail.com>
2017-11-04 23:22:58 +0100

    refactor to a structure compatible with github pages

--- docs/out/_next/6537651d-35c3-46a9-b60c-291346ee0199/page/_error/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_error', function() {
-            var comp = module.exports=webpackJsonp([3],[],[210]);
-            return { page: comp.default }
-          })
-        
---
+++ example/out/_next/6537651d-35c3-46a9-b60c-291346ee0199/page/_error/index.js
@@ -0,0 +1,4 @@
+window.__NEXT_REGISTER_PAGE('/_error', function() {
+  var comp = (module.exports = webpackJsonp([3], [], [210]))
+  return { page: comp.default }
+})
