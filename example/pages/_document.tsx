c7a3b84531b078de5a43385d581653f7a7b137da
Stian Didriksen <stipsan@gmail.com>
2018-04-15 03:19:33 +0200

    adding bulma

--- example/pages/_document.tsx
+++ example/pages/_document.tsx
@@ -15,7 +15,11 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css"
+          />
           {this.props.styleTags}
         </Head>
         <body>
