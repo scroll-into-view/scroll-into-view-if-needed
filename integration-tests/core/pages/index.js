9f78f3934b9461d96700b5a4b2c0294eab1080a2
Stian Didriksen <stipsan@gmail.com>
2018-04-24 19:37:06 +0200

    working on initial integration tests

---
+++ integration-tests/core/pages/index.js
@@ -0,0 +1,10 @@
+/* Without CSS Modules, maybe with PostCSS */
+
+import '../style.css'
+
+export default () => (
+  <div className="example">
+    <div className="bulls-eye" />
+    O Hai world!
+  </div>
+)
