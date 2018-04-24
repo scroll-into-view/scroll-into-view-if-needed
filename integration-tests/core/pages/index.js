e464b09083ec6c8827e4689e3eb52af2bff9556c
Stian Didriksen <stipsan@gmail.com>
2018-04-24 19:50:40 +0200

    test

--- integration-tests/core/pages/index.js
+++ integration-tests/core/pages/index.js
@@ -1,10 +1,35 @@
-/* Without CSS Modules, maybe with PostCSS */
+import { Component, Fragment, createRef } from 'react'
+import scrollIntoView from 'scroll-into-view-if-needed'
 
 import '../style.css'
 
+class Test extends Component {
+  target = createRef()
+
+  state = { test: undefined }
+
+  componentDidMount() {
+    const node = this.target.current
+    scrollIntoView(node)
+  }
+
+  render() {
+    return (
+      <div
+        className="bulls-eye"
+        ref={this.target}
+        data-test={this.state.test}
+      />
+    )
+  }
+}
+
 export default () => (
-  <div className="example">
-    <div className="bulls-eye" />
-    O Hai world!
-  </div>
+  <Fragment>
+    <div className="is-full-height" />
+    <div className="is-full-height">
+      <Test />
+    </div>
+    <div className="is-full-height" />
+  </Fragment>
 )
