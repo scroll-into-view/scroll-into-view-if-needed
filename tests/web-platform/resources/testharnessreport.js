7799b6df2a6d2268daa5b6d66d3349ecef71be4b
PrettierCI <commits@prettierci.com>
2018-05-12 22:21:12 +0000

    Sync with Prettier

--- tests/web-platform/resources/testharnessreport.js
+++ tests/web-platform/resources/testharnessreport.js
@@ -15,29 +15,36 @@
  */
 
 function dump_test_results(tests, status) {
-  var results_element = document.createElement("script");
-  results_element.type = "text/json";
-  results_element.id = "__testharness__results__";
+  var results_element = document.createElement('script')
+  results_element.type = 'text/json'
+  results_element.id = '__testharness__results__'
   var test_results = tests.map(function(x) {
-      return {name:x.name, status:x.status, message:x.message, stack:x.stack}
-  });
-  var data = {test:window.location.href,
-              tests:test_results,
-              status: status.status,
-              message: status.message,
-              stack: status.stack};
-  results_element.textContent = JSON.stringify(data);
+    return {
+      name: x.name,
+      status: x.status,
+      message: x.message,
+      stack: x.stack,
+    }
+  })
+  var data = {
+    test: window.location.href,
+    tests: test_results,
+    status: status.status,
+    message: status.message,
+    stack: status.stack,
+  }
+  results_element.textContent = JSON.stringify(data)
 
   // To avoid a HierarchyRequestError with XML documents, ensure that 'results_element'
   // is inserted at a location that results in a valid document.
   var parent = document.body
-      ? document.body                 // <body> is required in XHTML documents
-      : document.documentElement;     // fallback for optional <body> in HTML5, SVG, etc.
+    ? document.body // <body> is required in XHTML documents
+    : document.documentElement // fallback for optional <body> in HTML5, SVG, etc.
 
-  parent.appendChild(results_element);
+  parent.appendChild(results_element)
 }
 
-add_completion_callback(dump_test_results);
+add_completion_callback(dump_test_results)
 
 /* If the parent window has a testharness_properties object,
 * we use this to provide the test settings. This is used by the
@@ -45,13 +52,12 @@ add_completion_callback(dump_test_results);
 * rendering of results
 */
 try {
-  if (window.opener && "testharness_properties" in window.opener) {
-      /* If we pass the testharness_properties object as-is here without
+  if (window.opener && 'testharness_properties' in window.opener) {
+    /* If we pass the testharness_properties object as-is here without
        * JSON stringifying and reparsing it, IE fails & emits the message
        * "Could not complete the operation due to error 80700019".
        */
-      setup(JSON.parse(JSON.stringify(window.opener.testharness_properties)));
+    setup(JSON.parse(JSON.stringify(window.opener.testharness_properties)))
   }
-} catch (e) {
-}
+} catch (e) {}
 // vim: set expandtab shiftwidth=4 tabstop=4:
