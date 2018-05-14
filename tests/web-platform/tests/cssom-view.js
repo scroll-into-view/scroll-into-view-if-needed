4faa6463a381eb98ba6854dec2fbc09024debc90
Stian Didriksen <stipsan@gmail.com>
2018-05-14 22:40:06 +0200

    nightwatch works

--- tests/web-platform/tests/cssom-view.js
+++ tests/web-platform/tests/cssom-view.js
@@ -1,11 +1,26 @@
 module.exports = {
-  "Google's Search Functionality": function(browser) {
+  'scrollIntoView-shadow.html': function(browser) {
     browser
-      .url('https://www.google.com/ncr')
+      .url('http://localhost:3000/css/cssom-view/scrollIntoView-shadow.html')
       .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'BrowserStack\n')
-      .pause(1000)
-      .assert.title('BrowserStack - Google Search')
+      .assert.elementPresent('#summary .pass')
+      .assert.elementNotPresent('#summary .fail')
+      .end()
+  },
+  'scrollIntoView-smooth.html': function(browser) {
+    browser
+      .url('http://localhost:3000/css/cssom-view/scrollIntoView-smooth.html')
+      .waitForElementVisible('body', 1000)
+      .waitForElementPresent('#summary .pass', 10000)
+      .assert.elementNotPresent('#summary .fail')
+      .end()
+  },
+  'scrollintoview.html': function(browser) {
+    browser
+      .url('http://localhost:3000/css/cssom-view/scrollintoview.html')
+      .waitForElementVisible('body', 1000)
+      .assert.elementPresent('#summary .pass')
+      .assert.elementNotPresent('#summary .fail')
       .end()
   },
 }
