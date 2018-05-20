71abb4793b4bf9c161d2a3d318fcba0158152564
Stian Didriksen <stipsan@gmail.com>
2018-05-20 18:35:37 +0200

    switch to terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
-import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import { terser } from 'rollup-plugin-terser'
 
 const config = {
   input: 'src/index.ts',
-  name: 'scrollIntoView',
+  output: {
+    name: 'scrollIntoView',
+  },
   plugins: [
     babel({
       exclude: 'node_modules/**',
@@ -24,7 +26,7 @@ const config = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(uglify())
+  config.plugins.push(terser())
 }
 
 export default config
