25f57d31b15c41a09b8fbfcfc7608993f52b45dc
Stian Didriksen <stipsan@gmail.com>
2018-05-22 01:58:22 +0200

    add badge

--- tests/web-platform/tests/cssom-view.js
+++ tests/web-platform/tests/cssom-view.js
@@ -8,17 +8,20 @@ const tests = [
 ]
 const { browserName } = browser.desiredCapabilities
 console.log(browser.desiredCapabilities)
+
 describe(`${browserName}`, function() {
-  tests.forEach(testName => {
-    it(testName, () => {
-      browser.url(`http://localhost:3000/css/cssom-view/${testName}`)
-      browser.waitForVisible('#results', 10000)
-      browser.waitForText('#__testharness__results__', 10000)
-      const testResults = JSON.parse(
-        browser.getHTML('#__testharness__results__', false)
-      )
-      testResults.tests.forEach(testResult => {
-        assert.equal(testResult.message, null)
+  describe('cssom-view', () => {
+    tests.forEach(testName => {
+      it(testName, () => {
+        browser.url(`http://localhost:3000/css/cssom-view/${testName}`)
+        browser.waitForVisible('#results', 10000)
+        browser.waitForText('#__testharness__results__', 10000)
+        const testResults = JSON.parse(
+          browser.getHTML('#__testharness__results__', false)
+        )
+        testResults.tests.forEach(testResult => {
+          assert.equal(testResult.message, null)
+        })
       })
     })
   })
