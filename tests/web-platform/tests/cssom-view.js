be9c4e663bbd16cd373f8e7e18ae63b721186b4b
Stian Didriksen <stipsan@gmail.com>
2018-05-22 02:05:07 +0200

    test

--- tests/web-platform/tests/cssom-view.js
+++ tests/web-platform/tests/cssom-view.js
@@ -12,18 +12,16 @@ const {
 } = browser.desiredCapabilities
 
 describe(`${browserName}: ${browser_version}`, function() {
-  describe('cssom-view', () => {
-    tests.forEach(testName => {
-      it(testName, () => {
-        browser.url(`http://localhost:3000/css/cssom-view/${testName}`)
-        browser.waitForVisible('#results', 10000)
-        browser.waitForText('#__testharness__results__', 10000)
-        const testResults = JSON.parse(
-          browser.getHTML('#__testharness__results__', false)
-        )
-        testResults.tests.forEach(testResult => {
-          assert.equal(testResult.message, null)
-        })
+  tests.forEach(testName => {
+    it(testName, () => {
+      browser.url(`http://localhost:3000/css/cssom-view/${testName}`)
+      browser.waitForVisible('#results', 10000)
+      browser.waitForText('#__testharness__results__', 10000)
+      const testResults = JSON.parse(
+        browser.getHTML('#__testharness__results__', false)
+      )
+      testResults.tests.forEach(testResult => {
+        assert.equal(testResult.message, null)
       })
     })
   })
