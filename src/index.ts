e12c951ff7dcbd37ffc1c37bafb0b29ce5566941
Cody Olsen <stipsan@gmail.com>
2018-10-14 23:06:23 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,7 @@ function scrollIntoView<T>(
 function scrollIntoView(target: Element, options?: Options | boolean): void
 function scrollIntoView<T>(target: Element, options?: Options<T> | boolean) {
   // Browsers treats targets that aren't in the dom as a no-op and so should we
-  const targetIsDetached = !target.ownerDocument!.documentElement.contains(
+  const targetIsDetached = !target.ownerDocument!.documentElement!.contains(
     target
   )
 
